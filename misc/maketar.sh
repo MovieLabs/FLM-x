f01f5cdfa9c6a5818b245f30aa220dd5914ded92
Paul Jensen <pgj@movielabs.com>
2015-07-15 16:53:02 -0700

    cross-platform mods

--- misc/maketar.sh
+++ misc/maketar.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/sh
-cd ~pgj/code
-tar cpvjf flm.tgz --exclude=flm/node_modules\* --exclude=misc --exclude=tmp --exclude=save flm
+#!/usr/bin/bash
+if [[ $(basename `pwd`) != "flm" ]]
+then
+    echo "not in flm directory"
+    exit -1
+fi
+cd ..
+tar cpvjf ~/flm.tgz --exclude=flm/node_modules\* --exclude=tmp --exclude=save flm
